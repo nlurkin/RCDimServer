0e4fabb7b5fe5b2d19f06c6bece2229ee47d1d79
nlurkin <nicolas.lurkin@gmail.com>
2015-04-19 22:06:23 +0100

    Update version and HISTORY

--- Version.h
+++ Version.h
@@ -14,6 +14,6 @@ typedef struct NA62Version {
 	const int fPatch;
 } NA62Version_t;
 
-static NA62Version_t server_version = {2,3,1};
+static NA62Version_t server_version = {2,4,0};
 
 #endif /* VERSION_H_ */
